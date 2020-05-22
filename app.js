29fcec1b05f01a9035bf285c0c401c885ccfe454
aviki <toldi.victoria@gmail.com>
2020-05-22 21:54:43 +0200

    experimenting with bootstrap

--- app.js
+++ app.js
@@ -35,3 +35,18 @@ function init() {
     }
   }
 }
+
+//Bootstrap count button
+
+$(document).ready(function () {
+  $(".count").prop("disabled", true);
+  $(document).on("click", ".plus", function () {
+    $(".count").val(parseInt($(".count").val()) + 1);
+  });
+  $(document).on("click", ".minus", function () {
+    $(".count").val(parseInt($(".count").val()) - 1);
+    if ($(".count").val() == 0) {
+      $(".count").val(1);
+    }
+  });
+});
